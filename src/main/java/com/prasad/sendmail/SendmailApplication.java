1265f38192c45aa148c0745da4040640d0ccda56
KALI PRASAD <kancharlaguruprasad@gmail.com>
2023-01-08 11:05:17 +0530

    Add send mail application

---
+++ src/main/java/com/prasad/sendmail/SendmailApplication.java
@@ -0,0 +1,13 @@
+package com.prasad.sendmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendmailApplication.class, args);
+	}
+
+}
