3ea459ca09674317ccd6768d21b8b08907147561
KALI PRASAD <kancharlaguruprasad@gmail.com>
2023-01-08 11:06:14 +0530

    Add tests

---
+++ src/test/java/com/prasad/sendmail/SendmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasad.sendmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
